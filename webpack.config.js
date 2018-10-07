dfa3aa9b2488e838824a4e8a8402f1d3d439f285
Alexander Ivanov <yogaaaaa@yahoo.com>
2018-10-07 11:04:36 +0300

    v3: subscribe to store changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './v2/main.js',
+  entry: './v3/main.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, './')
