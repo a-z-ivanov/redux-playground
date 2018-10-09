e04abea319a74889e837c6fae250e4fa28722561
Ivanov <alexander.ivanov01@sap.com>
2018-10-09 12:37:37 +0300

    transition to web version

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './v3/main.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, './')
-  }
+	target: 'web',
+	mode: 'development',
+	entry: './v3/main.js',
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, './public')
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "/public")
+	}
 };
