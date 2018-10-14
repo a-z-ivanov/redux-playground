c9e5972700dcd6110a02642b9cfd84eee7b19090
Alexander Ivanov <yogaaaaa@yahoo.com>
2018-10-15 00:19:51 +0300

    defined default routes server side

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(pathToPublic));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(pathToPublic, 'index.html'));
+});
+
 app.listen(port, () => {
     console.log('Server is up!');
 });
